c6ed24244c574fc7a0248c475368183e890f9a8d
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2021-01-22 14:49:13 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import News from "./News";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/" id="en">
-                English
-              </Link>
-            </li>
-            <li>
-              <Link to="/hi" id="hi">
-                Hindi
-              </Link>
-            </li>
-            <li>
-              <Link to="/mr" id="mr">
-                Marathi
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <Switch>
-          <Route exact path={"/"}>
-            <News />
-          </Route>
-          <Route path={`/:lan`}>
-            <News />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
+const App = () => {
+  return <div id="main"></div>;
+};
 
 export default App;
